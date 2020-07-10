d7b87422a70d7a428be67da8b90fd1ec69feecc6
Ryder Chen <ryder_chen@infofab.com>
2020-07-10 15:03:44 +0800

    ADD Middleware

---
+++ NetCoreMVC/Extensions/CustomMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Extensions
+{
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseFirstMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FirstMiddleware>();
+        }
+    }
+    public class FirstMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FirstMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync($"{nameof(FirstMiddleware)} in. \r\n");
+
+            await _next(context);
+
+            await context.Response.WriteAsync($"{nameof(FirstMiddleware)} out. \r\n");
+        }
+    }
+}
--- NetCoreMVC/Startup.cs
+++ NetCoreMVC/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NetCoreMVC.Extensions;
 
 namespace NetCoreMVC
 {
@@ -21,7 +23,7 @@ namespace NetCoreMVC
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-      //此方法是用來將服務註冊到 DI 容器用的    
+        //此方法是用來將服務註冊到 DI 容器用的    
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -37,34 +39,122 @@ namespace NetCoreMVC
         /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            #region  Step 1 . Run
+            //Run 只會執行此次MiddleWare , 並不會往下走
+            //將多個要求委派鏈結在一起的方法是使用 Use
+            //next 參數代表管線中的下個委派 ，委派不將要求傳遞到下一個委派時，這就是所謂讓要求管線短路。
+            //app.Use(async (context, next) =>
+            //  {
+            //      await context.Response.WriteAsync("First Middleware in. \r\n");
+            //      await next.Invoke();
+            //      await context.Response.WriteAsync("First Middleware out. \r\n");
+            //  });
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Second Middleware in. \r\n");
+            //    await next.Invoke();
+            //    await context.Response.WriteAsync("Second Middleware out. \r\n");
+            //});
+
+
+            //app.Use(async (context, next) =>
+            //        {
+            //            await context.Response.WriteAsync("Third Middleware in. \r\n");
+            //            await next.Invoke();
+            //            await context.Response.WriteAsync("Third Middleware out. \r\n");
+            //        });
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello, Run World!  \r\n");
+            //});
+
+
+            #endregion  /Run
+
+            #region Step 2. Use        
+
+            /*
+                        app.Use(async (context, next) =>
+                        {
+                            await context.Response.WriteAsync("First Middleware in. \r\n");
+                            await next.Invoke();
+                            await context.Response.WriteAsync("First Middleware out. \r\n");
+                        });
+
+                        app.Use(async (context, next) =>
+                        {
+                            await context.Response.WriteAsync("Second Middleware in. \r\n");
+
+                            // 水管阻塞，封包不往後送
+                            var condition = false;
+                            if (condition)
+                            {
+                                //next 參數代表管線中的下個委派 ，委派不將要求傳遞到下一個委派時，這就是所謂讓要求管線短路。
+                                await next.Invoke();
+                            }
+
+                            await context.Response.WriteAsync("Second Middleware out. \r\n");
+                        });
+
+                        app.Use(async (context, next) =>
+                        {
+                            await context.Response.WriteAsync("Third Middleware in. \r\n");
+                            await next.Invoke();
+                            await context.Response.WriteAsync("Third Middleware out. \r\n");
+                        });
+
+                        app.Run(async (context) =>
+                        {
+                            await context.Response.WriteAsync("Hello World! \r\n");
+                        });
+                        */
+            #endregion /Use        
+
+            #region  Step3. Map
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("First Middleware in. \r\n");
+            //    await next.Invoke();
+            //    await context.Response.WriteAsync("First Middleware out. \r\n");
+            //});
+
+            //app.Map("/second", mapApp =>
+            //{
+            //    mapApp.Use(async (context, next) =>
+            //    {
+            //        await context.Response.WriteAsync("Second Middleware in. \r\n");
+            //        await next.Invoke();
+            //        await context.Response.WriteAsync("Second Middleware out. \r\n");
+            //    });
+            //    mapApp.Run(async context =>
+            //    {
+            //        await context.Response.WriteAsync("Second. \r\n");
+            //    });
+            //});
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello World! \r\n");
+            //});
+            #endregion  /Map
+
+            #region Step4. Extension 
+            app.UseMiddleware<FirstMiddleware>();   //Custom Middleware
+            app.UseFirstMiddleware();                             //Use Custom Middleware Extension 
+            app.Run(async context =>
             {
-                app.UseDeveloperExceptionPage();                        //啟用開發人員例外狀況
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");     //例外處理時 導頁到Error 
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();                              //強制瀏覽器要使用HTTPS 造訪網站
-            }
-
-            app.UseHttpsRedirection();                  //重新以Https 導向網站
-
-            app.UseStaticFiles();           //靜態檔案引入 CSS, Img ,js
-
-            //會將Route參照加入Middleware , Middleware 會去依照useEndpoint所設定的位置依據Request 進行選擇最好的路由參照
-            app.UseRouting();
-
-
-            app.UseAuthorization();     //簡易授權
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                await context.Response.WriteAsync("Hello World! \r\n");
             });
+            #endregion /Step4. Extension 
+
+            #region ServiceFilter
+            //app.UseMiddleware<MiddlewareService>(); 
+
+            #endregion ServiceFilter
+
         }
     }
 }
