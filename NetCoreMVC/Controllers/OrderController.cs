0187ed71dd184d351a45a6fe432a2680d2bb492c
Ryder Chen <ryder_chen@infofab.com>
2020-07-13 16:29:15 +0800

    Add MVC Structure

---
+++ NetCoreMVC/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMVC.Models;
+
+namespace NetCoreMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(GetOrders());
+        }
+        public List<modelOrder> GetOrders()
+        {
+            List<modelOrder> orders = new List<modelOrder>();
+            for (int i = 1; i < 10; i++)
+            {
+                orders.Add(new modelOrder
+                {
+                    ID = Guid.NewGuid(),
+                    OrderNo = i.ToString(),
+                    OrderDT = DateTime.Now,
+                    CustomerName = $"Cust{i}",
+                    EachPrice = 10,
+                    Number = 5,
+                });
+            }
+            return orders;
+        }
+    }
+}
---
+++ NetCoreMVC/Models/modelOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Models
+{
+    public class modelOrder
+    {
+        public Guid ID { get; set; }
+        public string OrderNo { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDT { get; set; }
+        public decimal Number { get; set; }
+        public decimal EachPrice { get; set; }
+
+
+    }
+}
